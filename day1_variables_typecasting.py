f6ef4d590f09a053e22ff3cafbb917e60f3cd8e7
hashirsakimdad <hashirsakimdad@gmail.com>
2025-06-24 13:13:11 +0500

    Add Day 1 task: variables and typecasting

---
+++ day1_variables_typecasting.py
@@ -0,0 +1,12 @@
+# Day 1 – Python Internship Task
+
+# Taking input from user
+int_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+string_input = input("Enter a string: ")
+
+# Displaying the values with formatted print
+print("\n--- Formatted Output ---")
+print(f"Integer value: {int_input}")
+print(f"Float value: {float_input}")
+print(f"String value: '{string_input}'")
